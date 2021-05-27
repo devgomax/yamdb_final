3e26b13a1d2668a7b513ba1800b8f34faabd3fde
devgomax <maxgoal00@gmail.com>
2021-05-27 11:50:29 +0600

    little fixes to pass the tests

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -66,7 +66,8 @@ WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get('DB_ENGINE'),
+        # 'ENGINE': os.environ.get('DB_ENGINE'),
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
